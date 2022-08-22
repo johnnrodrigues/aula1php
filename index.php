e07dfe722908fd810aa180d4547f00a935a5d0a9
johnnrodrigues <johnn_rodrigues@hotmail.com>
2022-08-22 16:01:29 -0300

    Sintaxe basica de comandos FOR, IF, VARIAVEL e ECHO

--- index.php
+++ index.php
@@ -10,7 +10,54 @@
 </head>
 <body>
     <?php
-        echo "Boa"; 
+    #Comentario
+    // comentario
+    /*
+        code block
+    */
+        echo "Boa";
+        echo "<br>";
+        echo "<h1>Hello World2 </h1>";
+
+        $nome = "Johnn";
+        $idade = 99;
+        $altura = 1.80;
+
+        $prova1 = 8;
+        $prova2 = 10;
+        $soma = $prova1 + $prova2;
+        $media = $soma / 2;
+
+        $multiplicacao = $prova1 * $prova2;
+        $subtracao = $prova1 - $prova2;
+        $faltas = 21;
+
+        CONST NOMECOMPLETO = "JOHNNY RODRIGUES EVANGELISTA";
+        echo "<p>". NOMECOMPLETO . "</p>";
+        echo "<br>";
+
+        if ($media > 6 && $faltas <= 20) {
+            echo "Sua média foi de $media.<br>
+            e você teve $faltas, portanto...
+            Parabéns, voce foi aprovado!";
+        } else {
+            echo "Sua média foi de $media.<br>
+            e você teve $faltas, portanto...
+            Você foi reprovado!";
+        }
+
+
+        for ($i = 1; $i < 5197; $i++) {
+            if($i % 2 == 0) {
+                echo $i. " é par <br>";
+            
+        } else {
+            echo $i." é impar<br>";
+        }
+    }
+
+
+
     ?>
     
     <h1>Hello World </h1>
